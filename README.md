286970417ebb5b8b14671ceef01795b807489815
akpi816218 <816218@seq.org>
2023-06-13 12:32:46 -0700

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# discog-web
+
+The website for [DisCog](https://github.com/akpi816218/discog)
---
+++ assets/js/count.js
@@ -0,0 +1,18 @@
+fetch(
+	'https://cors-anywhere.herokuapp.com/https://discog.akpi816218.repl.co/api/',
+	{
+		method: 'GET',
+		headers: [
+			['Content-Type', 'application/json']
+			// ['Access-Control-Allow-Origin', '*']
+		]
+	}
+)
+	.then((res) => res.json())
+	.then((data) => {
+		document.getElementById('server-count').innerText = data.guildCount;
+		document.getElementById('command-count').innerText =
+			data.commandCount ?? '20';
+		// document.getElementById('user-count').innerText = data.userCount;
+	})
+	.catch((err) => console.error(err));
